7db81db08e3186e2bd52cd2535bbe512c566b2e6
Tarkan Cicek <tarkancicek@MacBook-Air.local>
2022-04-26 21:36:37 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Degisiklik yapildi! Yine. yok artik.
         </p>
         <a
           className="App-link"
